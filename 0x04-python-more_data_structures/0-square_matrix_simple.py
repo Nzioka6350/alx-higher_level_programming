633d036a91ad64312b36bb59eed2b3491401826d
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-09-13 02:21:36 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
-#Task: 0-square_matrix_simple.py
-def square_matrix_simple(matrix=[]):
 
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix to store the squared values
+    squared_matrix = []
+
+    # Iterate over the rows of the input matrix
+    for row in matrix:
+        # Create a new row for the squared values
+        squared_row = []
+
+        # Iterate over the elements in the current row and square each element
+        for element in row:
+            squared_element = element ** 2
+            squared_row.append(squared_element)
+
+        # Append the squared row to the squared matrix
+        squared_matrix.append(squared_row)
+
+    return squared_matrix
